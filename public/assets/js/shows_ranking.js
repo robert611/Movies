b4f0f92c4aac6f1db951d7fae7a708fdc7494ea5
robert611 <cyrkiel86@interia.pl>
2021-03-14 18:24:03 +0100

    Add functionality for comparing shows in the homepage

---
+++ public/assets/js/shows_ranking.js
@@ -0,0 +1,148 @@
+let shows = document.getElementsByClassName('show-to-compare-picture');
+
+Array.from(shows).forEach((show) => {
+    show.addEventListener('click', vote);
+});
+
+function chooseShowsToCompare()
+{
+    let showsContainer = document.getElementById('container-for-shows-to-compare');
+
+    hideRankingContainerChildren();
+
+    showRankingSpinner();
+
+    fetch('/api/ranking/find/shows/to/compare')
+        .then((response) => {
+            return response.json();
+        })
+        .then((shows) => {
+
+            shows = [JSON.parse(shows[0]), JSON.parse(shows[1])];
+
+            hideRankingContainerChildren();    
+
+            shows.map((show) => {
+                showsContainer.appendChild(createShowToCompareWidget(show));
+            });
+        
+            let showsToComparePictures = document.getElementsByClassName('show-to-compare-picture');
+
+            Array.from(showsToComparePictures).forEach((picture) => {
+                picture.addEventListener('click', vote);
+            });
+        }); 
+}
+
+function vote(event)
+{
+    event.preventDefault();
+
+    showRankingSpinner();
+
+    let pictures = document.querySelectorAll('.show-to-compare-picture img');
+    let winnerShowName = event.target.getAttribute('data-showName');
+    let winnerDatabaseTableName = event.target.getAttribute('data-tableName');
+
+    let data = new FormData();
+
+    data.append('show_name', winnerShowName);
+    data.append('database_table_name', winnerDatabaseTableName);
+
+    fetch('/ranking/vote/up', {
+        method: 'POST',
+        body: data
+    });
+
+    Array.from(pictures).map((picture) => {        
+        if (picture.dataset.tablename !== winnerDatabaseTableName)
+        {
+            voteDown(picture);
+        }
+    });
+
+    chooseShowsToCompare();
+}
+
+function voteDown(looser) {
+    let showName = looser.getAttribute('data-showName');
+    let databaseTableName = looser.getAttribute('data-tableName');
+ 
+    let data = new FormData();
+    
+    data.append('show_name', showName);
+    data.append('database_table_name', databaseTableName);
+
+    fetch('/ranking/vote/down', {
+        method: 'POST',
+        body: data
+      });
+}
+
+function showRankingSpinner()
+{
+    let showsContainer = document.getElementById('container-for-shows-to-compare');
+
+    let spinner = createRankingSpinnerWidget();
+
+    showsContainer.appendChild(spinner);
+}
+
+function createShowToCompareWidget(show)
+{
+    let wrapper = document.createElement('article');
+    wrapper.setAttribute('class', 'col-xs-12 col-sm-6 col-md-6 p-2');
+
+    let a = document.createElement('a');
+    a.setAttribute('href', '#');
+    a.setAttribute('class', 'show-to-compare-picture');
+
+    let img = document.createElement('img');
+    img.setAttribute('src', '/assets/pictures/shows/' + show.picture);
+    img.setAttribute('data-id', show.id);
+    img.setAttribute('data-showName', show.name);
+    img.setAttribute('data-tableName', show.databaseTableName);
+
+    let titleDiv = document.createElement('div');
+    titleDiv.setAttribute('class', 'show-to-compare-title-div');
+
+    titleDiv.textContent = show.name;
+
+    a.appendChild(img);
+
+    wrapper.appendChild(a);
+    wrapper.appendChild(titleDiv);
+
+    return wrapper;
+}
+
+function createRankingSpinnerWidget()
+{
+    let center = document.createElement('center');
+    center.setAttribute('class', 'mx-auto');
+    center.style.marginTop = '50px';
+    center.style.marginBottom = '50px';
+
+    let spinner = document.createElement('div');
+    spinner.setAttribute('class', 'spinner-border text-secondary');
+
+    let span = document.createElement('span');
+    span.setAttribute('class', 'sr-only');
+
+    span.textContent = 'Loading...';
+
+    spinner.appendChild(span);
+    center.appendChild(spinner);
+
+    return center;
+}
+
+function hideRankingContainerChildren() {
+    let shows = document.getElementById('container-for-shows-to-compare');
+
+    Array.from(shows.children).forEach((child) => {
+        child.remove();
+    });
+}
+
+chooseShowsToCompare();
---
+++ src/Controller/ShowsRankingController.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use App\Entity\ShowRanking;
+use App\Entity\Shows;
+
+class ShowsRankingController extends AbstractController
+{
+    /**
+     * @Route("/api/ranking/find/shows/to/compare", name="ranking_find_shows_to_compare")
+     */
+    public function findShowsToCompare(): Response
+    {
+        $showsRepository = $this->getDoctrine()->getRepository(Shows::class);
+
+        $shows = $showsRepository->findAll();
+
+        $showsToCompare = array(); 
+
+        while (count($showsToCompare) < 2)
+        {
+            $rand = rand(0, count($shows) - 1);
+
+            $randomShow = $shows[$rand];
+
+            if (isset($showsToCompare[0])) 
+            {
+                if ($showsToCompare[0]->getId() !== $randomShow->getId())
+                {
+                    $showsToCompare[] = $randomShow;
+                }
+            }
+            else
+            {
+                $showsToCompare[] = $randomShow;
+            }
+        }
+
+        $defaultContext = [
+            AbstractNormalizer::IGNORED_ATTRIBUTES => ['user', 'description', 'createdAt', 'updatedAt']
+        ];
+
+        $encoders = [new JsonEncoder()];
+        $normalizers = [new ObjectNormalizer(null, null, null, null, null, null, $defaultContext)];
+
+        $serializer = new Serializer($normalizers, $encoders);
+
+        return new JsonResponse([$serializer->serialize($showsToCompare[0], 'json'), $serializer->serialize($showsToCompare[1], 'json')]);
+    }
+
+    /**
+     * @Route("/ranking/vote/up", name="ranking_vote_up")
+     */
+    public function voteUp(Request $request): Response
+    {
+        $showRankingRepository = $this->getDoctrine()->getRepository(ShowRanking::class);
+            
+        $databaseTableName = $request->request->get('database_table_name');
+        $showName = $request->request->get('show_name');
+
+        $show = $showRankingRepository->findOneBy(['show_database_table_name' => $databaseTableName]);
+
+        $entityManager = $this->getDoctrine()->getManager();
+
+        if (!is_object($show))
+        {
+            $showRanking = new ShowRanking();
+            $showRanking->setShowName($showName);
+            $showRanking->setShowDatabaseTableName($databaseTableName);
+            $showRanking->setVotesUp(1);
+            $showRanking->setVotesDown(0);
+
+            $entityManager->persist($showRanking);
+        } else {
+            $score = $show->getVotesUp();
+            $show->setVotesUp($score + 1);
+
+            $entityManager->persist($show);
+        }
+
+        $entityManager->flush();
+
+        return new JsonResponse(true);
+    }
+
+    /**
+     * @Route("/ranking/vote/down", name="ranking_vote_down")
+     */
+    public function voteDown(Request $request): Response
+    {
+        $showRankingRepository = $this->getDoctrine()->getRepository(ShowRanking::class);
+           
+        $databaseTableName = $request->request->get('database_table_name');
+        $showName = $request->request->get('show_name');
+     
+        $show = $showRankingRepository->findOneBy(['show_database_table_name' => $databaseTableName]);
+
+        $entityManager = $this->getDoctrine()->getManager();
+
+        if (!is_object($show)) 
+        {  
+            $showRanking = new ShowRanking();
+            $showRanking->setShowName($showName);
+            $showRanking->setShowDatabaseTableName($databaseTableName);
+            $showRanking->setVotesUp(0);
+            $showRanking->setVotesDown(1);
+
+            $entityManager->persist($showRanking);
+        } else {
+            $score = $show->getVotesDown();
+            $show->setVotesDown($score + 1);
+
+            $entityManager->persist($show);
+        }
+
+        $entityManager->flush();
+
+        return new JsonResponse(true);
+    }
+}
