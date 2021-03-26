36fb1ef87db30232887547a1aa81be2e67a6035f
robert611 <cyrkiel86@interia.pl>
2021-03-26 20:18:39 +0100

    Add tests for registration controller

---
+++ tests/Controller/RegistrationControllerTest.php
@@ -0,0 +1,71 @@
+<?php 
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use App\Repository\UserRepository;
+use App\Entity\User;
+
+class RegistrationControllerTest extends WebTestCase
+{
+    public $client = null;
+
+    public function setUp(): void
+    {
+        $this->client = static::createClient();
+    }
+
+    /**
+     * @runInSeparateProcess
+     */
+    public function testIfRegistrationPageIsSuccessfull()
+    {
+        $this->client->request('GET', '/register');
+        
+        $this->assertEquals(200, $this->client->getResponse()->isSuccessful());
+        $this->assertSelectorTextContains('html h1', 'Register');
+    }
+
+    /**
+     * @runInSeparateProcess
+     */
+    public function testIfUserCanRegister()
+    {
+        $crawler = $this->client->request('GET', '/register');
+
+        $form = $crawler->selectButton('Register')->form();
+
+        $form['registration_form[email]'] = 'test_registration_email@test.pl';
+        $form['registration_form[plainPassword]'] = 'password';
+        $form['registration_form[username]'] = 'test_registration_user';
+
+        $crawler = $this->client->submit($form);
+
+        $this->client->request('GET', '/');
+        $this->assertSelectorTextNotContains('html', 'Exception');
+        $this->assertSelectorTextContains('html', 'Hello: test_registration_user');
+
+        $users = static::$container->get('doctrine')->getRepository(User::class)->findAll();
+        $user = $users[count($users) - 1];
+
+        $entityManager = static::$container->get('doctrine.orm.entity_manager');
+
+        $entityManager->remove($user);
+        $entityManager->flush();
+        
+        $this->assertEquals($form->get('registration_form[email]')->getValue(), $user->getEmail());
+        $this->assertEquals($form->get('registration_form[username]')->getValue(), $user->getUsername());
+    }
+
+    /**
+     * @runInSeparateProcess
+     */
+    public function testIfLogedInUserCannotRegister()
+    {
+        $this->client->loginUser(static::$container->get(UserRepository::class)->findAll()[0]);
+
+        $crawler = $this->client->request('GET', '/register');
+
+        $this->assertResponseRedirects('/');
+    }
+}
